415c0ec7e5e738ed5182e3b6c542c9c3386a589f
Anthony <anthonymanoel12@gmail.com>
2022-07-31 22:15:43 -0300

    add MYsql server

---
+++ project/server/index.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "Login Page - Mind",
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  db.query("SELECT * FROM usuarios WHERE email = ?", [email], (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+    if (result.length == 0) {
+      bcrypt.hash(password, saltRounds, (err, hash) => {
+        db.query(
+          "INSERT INTO usuarios (email, password) VALUE (?,?)",
+          [email, hash],
+          (error, response) => {
+            if (err) {
+              res.send(err);
+            }
+
+            res.send({ msg: "User sucessfully registered" });
+          }
+        );
+      });
+    } else {
+      res.send({ msg: "Email already exists" });
+    }
+  });
+});
+
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  db.query("SELECT * FROM usuarios WHERE email = ?", [email], (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+    if (result.length > 0) {
+      bcrypt.compare(password, result[0].password, (error, response) => {
+        if (error) {
+          res.send(error);
+        }
+        if (response) {
+          res.send({ msg: "User logged" });
+        } else {
+          res.send({ msg: "Incorrect Password" });
+        }
+      });
+    } else {
+      res.send({ msg: "User don't exists" });
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log("running at 3001");
+});
